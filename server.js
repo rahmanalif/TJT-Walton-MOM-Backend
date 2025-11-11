98eb20f95c82dfdf7a55a52723602d42093198f3
rahmanalif <rahman.alif.007@gmail.com>
2025-11-11 21:32:13 +0600

    Fix port console log

--- server.js
+++ server.js
@@ -41,6 +41,6 @@ app.use((err, req, res, next) => {
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
   console.log(`Environment: ${process.env.NODE_ENV || 'development'}`);
 });
